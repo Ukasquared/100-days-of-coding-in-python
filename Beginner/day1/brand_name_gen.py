8ac95fd84ffc8dddb4f04c98dbc6632da30cfb5c
Ukasquared <ukachukwujudith95@gmail.com>
2023-11-26 18:28:25 +0100

    day1

---
+++ Beginner/day1/brand_name_gen.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+print("welcome to the Band Name Generator")
+pet_name = input("What's your pet's name?\n")
+name = input("What's name of the city you grew up in?\n")
+print("Your band name could be " + name + " " + pet_name)
